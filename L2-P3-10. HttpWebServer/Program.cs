eb69151402e555952d5fd71e579422ef5380d6e1
p.titov <p.titov@TITOV-P>
2019-08-29 16:08:15 +0300

    WebSockets.

--- L2-P3-10. HttpWebServer/Program.cs
+++ L2-P3-10. HttpWebServer/Program.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.WebSockets;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace L2_P3_10.HttpWebServer
 {
@@ -17,7 +21,7 @@ namespace L2_P3_10.HttpWebServer
         static void Main(string[] args)
         {
             Server ws = new Server("http://*:8881/");
-            ws.Run();
+            ws.RunAsync();
 
             Console.WriteLine("Press a key to quit.");
             Console.ReadKey();
@@ -30,11 +34,15 @@ namespace L2_P3_10.HttpWebServer
     class Server: IDisposable
     {
         private HttpListener listener;
+        private List<WebSocket> clients = new List<WebSocket>();
+
 
         private string serverDirectory;
 
         const string mainPage = "index.html";
         const string participantsFile = "Participants.json";
+
+        DateTime dateModificat = DateTime.Now;
 
 
         public Server(string URI)
@@ -67,17 +75,15 @@ namespace L2_P3_10.HttpWebServer
             }
         }
 
-        public void Run()
+        public async void RunAsync()
         {
-            ThreadPool.QueueUserWorkItem(o =>
-            {
-                Console.WriteLine("Webserver running...");
+            Console.WriteLine("Webserver running...");
 
-                while (listener.IsListening)
-                {
-                    ThreadPool.QueueUserWorkItem(WriteToClient, listener.GetContext());
-                }
-            });
+            while (listener.IsListening)
+            {
+                WriteToClient(await listener.GetContextAsync());
+            }
+
         }
 
         public void Stop()
@@ -93,10 +99,9 @@ namespace L2_P3_10.HttpWebServer
 
 
 
-        private void WriteToClient(object obj)
+        private async void WriteToClient(HttpListenerContext context)
         {
 
-            var context = obj as HttpListenerContext;
             try
             {
                 if (context == null)
@@ -107,51 +112,123 @@ namespace L2_P3_10.HttpWebServer
                 HttpListenerResponse response = context.Response;
                 HttpListenerRequest request = context.Request;
 
-                if (context.Request.HttpMethod.Equals("GET"))
+                if (request.IsWebSocketRequest)
                 {
-                    string filePath = GetPath(request.Url.AbsolutePath);
-                    if (!File.Exists(filePath))
+
+                    HttpListenerWebSocketContext webSocketContext = await context.AcceptWebSocketAsync(null);
+                    WebSocket webSocket = webSocketContext.WebSocket;
+
+                    clients.Add(webSocket);
+
+                    while (webSocket.State == WebSocketState.Open)
                     {
-                        response.StatusCode = 404;
-                        return;
+                        var buffer = new ArraySegment<Byte>(new byte[1024]);
+                        var result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+
+                        for (int i = 0; i < clients.Count; i++)
+                        {
+                            WebSocket client = clients[i];
+                            try
+                            {
+                                if (client.State == WebSocketState.Open)
+                                {
+                                    await client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                                }
+                            }
+
+                            catch (ObjectDisposedException)
+                            {
+                                try
+                                {
+                                    clients.Remove(client);
+                                    i--;
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                    Console.WriteLine(ex.StackTrace);
+                                }
+                            }
+                        }
                     }
 
-                    string ifModifiedSince = request.Headers.Get("if-Modified-Since");
-                    DateTime dateTimeModified;
-                    
-                    if (ifModifiedSince == null || ( DateTime.TryParse(ifModifiedSince, out dateTimeModified) && dateTimeModified.AddMinutes(2) < DateTime.Now ) )
-                    {
-                        SetLastModified(response);
-                    }
-                    else
-                    {
-                        response.StatusCode = 304;
-                        return;
-                    }
-
-                    using (StreamReader file = new StreamReader(filePath))
-                    {
-                        string responseStr = file.ReadToEnd();
-
-                        responseStr = Participants.Replace(participantsFile, responseStr);
-
-                        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr);
-                        response.ContentLength64 = buffer.Length;
-                        Stream output = response.OutputStream;
-                        output.Write(buffer, 0, buffer.Length);
-                        output.Close();
-                    }
-                  
                 }
                 else
                 {
-                    response.StatusCode = 501;
-                    return;
+                    if (context.Request.HttpMethod.Equals("GET"))
+                    {
+                        if (request.Url.AbsolutePath == "/participants_list")
+                        {
+                            string responseStr = $"<ol>{Participants.HTMLView(participantsFile)}</ol>";
+                            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr);
+                            response.ContentLength64 = buffer.Length;
+                            Stream output = response.OutputStream;
+                            output.Write(buffer, 0, buffer.Length);
+                            output.Close();
+                        }
+                        else
+                        {
+                            string filePath = GetPath(request.Url.AbsolutePath);
+                            if (!File.Exists(filePath))
+                            {
+                                response.StatusCode = 404;
+                                return;
+                            }
+
+                            string ifModifiedSince = request.Headers.Get("if-Modified-Since");
+                            DateTime dateTimeModified;
+
+                            if (ifModifiedSince == null || (DateTime.TryParse(ifModifiedSince, out dateTimeModified) && (dateTimeModified.AddMinutes(2) < DateTime.Now|| dateModificat > dateTimeModified)))
+                            {
+                                SetLastModified(response);
+                            }
+                            else
+                            {
+                                response.StatusCode = 304;
+                                return;
+                            }
+
+                            using (StreamReader file = new StreamReader(filePath))
+                            {
+                                string responseStr = file.ReadToEnd();
+
+                                responseStr = Participants.Replace(participantsFile, responseStr);
+
+                                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr);
+                                response.ContentLength64 = buffer.Length;
+                                Stream output = response.OutputStream;
+                                output.Write(buffer, 0, buffer.Length);
+                                output.Close();
+                            }
+                        }
+                    }
+
+                    else if (context.Request.HttpMethod.Equals("POST"))
+                    {
+                        if (request.Url.AbsolutePath == "/vote")
+                        {
+                            Participants.Add(participantsFile, request);
+                            dateModificat = DateTime.Now;
+                            response.Redirect("participants.html");
+                        }
+                        else
+                        {
+                            response.StatusCode = 404;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        response.StatusCode = 501;
+                        return;
+                    }
                 }
+
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
             }
             finally
             {
@@ -209,7 +286,19 @@ namespace L2_P3_10.HttpWebServer
             }
             return p as Participants;
         }
+
+        public static void Write(string path, Participants p)
+        {
+            if (!File.Exists(path))
+                return ;
         
+            using (StreamWriter fs = new StreamWriter(path))
+            {
+                fs.Write(JsonConvert.SerializeObject(p));
+            }
+
+        }
+
         public static string HTMLView(string path)
         {
             string HTML = "";
@@ -228,6 +317,22 @@ namespace L2_P3_10.HttpWebServer
                 responseStr = responseStr.Replace("{{participants}}", participantsHTML);
             }
             return responseStr;
+
+        }
+
+        public static void Add(string path, HttpListenerRequest request)
+        {
+            StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding);
+            string s = reader.ReadToEnd();
+
+            NameValueCollection query = HttpUtility.ParseQueryString(s);
+
+            if(query["attend"] == "on")
+            {
+                Participants participants = Participants.Read(path);
+                participants.ListParticipants.Add(query["name"]);
+                Write(path, participants);
+            }
 
         }
     }
